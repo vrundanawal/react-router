f0b9c78fced9a2e77de4959cb7f53d50abc2b532
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-03-21 12:35:18 +0000

    how to access parameters from url with useParams hook implementation

---
+++ src/components/User/User.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const User = () => {
+  const { userid } = useParams();
+  return (
+    <div className="bg-gray-600 text-white text-3xl p-4 text-center">User : {userid}</div>
+  );
+};
+
+export default User;
+
+// import React from 'react'
+// import { useParams } from 'react-router-dom'
+
+// function User() {
+//     const {userid} = useParams()
+//   return (
+//     <div >User: {userid}</div>
+//   )
+// }
+
+// export default User
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx';
+import User from './components/User/User.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,6 +37,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
+      <Route path='/user/:userid' element={<User/>}/>
 
     </Route>
   )
